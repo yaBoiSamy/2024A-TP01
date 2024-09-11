fcd5ff0d25c1112082e6da9232a0660d097b6f8e
Adame tabet <adametbt12@gmail.com>
2024-09-11 16:53:53 -0400

    ex1 terminé

--- ex1.py
+++ ex1.py
@@ -1,5 +1,10 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input('Quel est son nom? ')
+date = input('Quelle est la date du record? ')
+discipline = input('Dans quelle discipline? ')
+categorie = input('Dans quelle catégorie spécifique? ')
+record = input('Quel est le record? ')
+
+print(f'Nouveau record: \n -------- \n {date} \n {discipline} - {categorie} \n {athlete} {country} - {record}')
