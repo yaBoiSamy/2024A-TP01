f377465487374fefba04d8bff73fe0fd0833b394
Samuel Bélanger <samprogrammation@gmail.com>
2024-09-11 17:09:06 -0400

    ex4 terminé

--- ex4.py
+++ ex4.py
@@ -3,4 +3,14 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+print("Pourcentage de batterie:", end=" ")
+pourcentage = int(input())
+map = { 50:2, 25:0.5, 10:1, 5:2.5, 0:6 }
+autonomie = 0
+
+for key in sorted(list(map.keys()))[::-1]:
+    if pourcentage > key:
+        autonomie += (pourcentage-key)*map[key]
+    pourcentage = key
+
+print(f"La distance possible est de {autonomie}km")
