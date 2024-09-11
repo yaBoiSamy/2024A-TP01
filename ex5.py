045e47f28f240fecf74baf0455d2b786f289d557
Samuel Bélanger <samprogrammation@gmail.com>
2024-09-11 17:37:32 -0400

    ex5 terminé

--- ex5.py
+++ ex5.py
@@ -1,5 +1,15 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+print("Résultat du pays:", end=" ")
+medals = {"G":0, "S":0, "B":0}
+for medal in input():
+    if medal in medals:
+        medals[medal]+=1
+    else:
+        medals = None
+if medals != None:
+    print(f"Médailles:\n- Or: {medals['G']}\n- Argent: {medals['S']}\n- Bronze: {medals['B']}")
+else:
+    print("Veuillez entrer une chaîne valide.")
+
