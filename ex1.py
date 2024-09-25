427f0de03e2654a30525200f70d7fb9da4b50989
Samuel Bélanger <samprogrammation@gmail.com>
2024-09-25 16:42:19 -0400

    _

--- ex1.py
+++ ex1.py
@@ -1,6 +1,9 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
-#euuubuygdviygedivb
+athlete = input('Quel est son nom ? ')
+date = input('Date du record ? ')
+discipline = input('Dans quelle discipline ? ')
+categorie = input('Dans une catégorie spécifique ? ')
+record = input('Quel est le record ? ')
+print(f'\nNouveau Record:\n--------------------\n{date} - {discipline} - {categorie}:\n\t{athlete} ({country}) - {record}')
--- ex5.py
+++ ex5.py
@@ -1,5 +1,10 @@
-#TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
-#      Attention si la chaîne est invalide, un message d'erreur est attendu.
 
-country = ...
-code_medals = ...
+print('Pays concerné ? ')
+pays = input()
+print("Chaine représentant les médailles ?", end=" ")
+medals = input()
+gold, silver, bronze = medals.count("G"), medals.count("S"), medals.count("B")
+if len(medals) > gold+silver+bronze:
+    print("Veuillez entrer une chaîne valide.")
+else:
+    print(f"{pays}Médailles:\n- Or: {gold}\n- Argent: {silver}\n- Bronze: {bronze}")
